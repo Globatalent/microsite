81db9a2aa469fc8852e4ab2f0ce882f2ac4b5ece
ruben <ruben@globatalent.com>
2019-05-24 19:04:25 +0200

    Updates

---
+++ precache-manifest.2bd273ff48de8b2373354fc86b6e3130.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d3e0a24788ff8f5c7610e2905774be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5595b7819b9b77bf1378",
+    "url": "/static/css/main.be046a40.chunk.css"
+  },
+  {
+    "revision": "f53c8c22b2c9c935d578",
+    "url": "/static/js/2.650f93fd.chunk.js"
+  },
+  {
+    "revision": "5595b7819b9b77bf1378",
+    "url": "/static/js/main.41d5e329.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "0ca3d4c753d5b3fd33639cef7b76838e",
+    "url": "/static/media/bck_1.0ca3d4c7.png"
+  },
+  {
+    "revision": "f1a4524485c58c5ced312afd68494837",
+    "url": "/static/media/bck_2.f1a45244.png"
+  },
+  {
+    "revision": "95b774e13ab22f0c959df3fc6fa816dc",
+    "url": "/static/media/bck_3.95b774e1.png"
+  },
+  {
+    "revision": "93c3d7655bd879ee7dd1264889fdd7d3",
+    "url": "/static/media/hero.93c3d765.png"
+  },
+  {
+    "revision": "7b95a71392adbc23b71782804427b0db",
+    "url": "/static/media/img_1.7b95a713.png"
+  },
+  {
+    "revision": "548d6ffb21f58a7b1dec77f3ef660ff4",
+    "url": "/static/media/img_2.548d6ffb.png"
+  },
+  {
+    "revision": "8e5ba06d975175a9a707f241b44a3242",
+    "url": "/static/media/img_3.8e5ba06d.png"
+  },
+  {
+    "revision": "f198cf423acf3707bcdbad66828ffc8d",
+    "url": "/static/media/prog_1.f198cf42.png"
+  },
+  {
+    "revision": "d1b02a35027700b431c96a5ae2911970",
+    "url": "/static/media/prog_2.d1b02a35.png"
+  },
+  {
+    "revision": "502ebee2279aa5826c89d2ac66742f34",
+    "url": "/static/media/prog_3.502ebee2.png"
+  }
+]);
